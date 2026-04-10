f88675c42801638d47c5103b387719de78a59442
Cardini Agustín <cardiniagustin8@gmail.com>
2026-04-10 18:24:36 -0300

    Actualizar programa04.cs

--- Programas Practica Cardini Agustín/Programa04/programa04.cs
+++ Programas Practica Cardini Agustín/Programa04/programa04.cs
@@ -6,15 +6,39 @@ class Program
     {
         Console.WriteLine("--- BLOQUE 3: TEXTO Y CARACTERES ---");
 
-            // Cadena de texto (string) - Comillas dobles ""
+            //1. Cadena de texto (string) - Comillas dobles ""
             string nombre = "Juan";
             string apellido="Lopez";
             Console.WriteLine($">>> TIPO TEXTO (string): {nombre} {apellido}");
+           //2.Recorrer un string
+string palabra = "Hola";
 
-            // Carácter único (char) - Comillas simples ''
+foreach (char c in palabra)
+{
+    Console.WriteLine("Char: " + c);
+}
+
+            //3. Carácter único (char) - Comillas simples ''
             char inicialNombre = 'J';
             char inicialApell = 'L';
             Console.WriteLine($">>> TIPO CARÁCTER (char): {inicialNombre} - {inicialApell}");
+            //4. Char y codigo ASCII
+char letra = 'A';
+int codigoAscii = (int)letra;
+            //5. Concatenacion de Chars
+char letra1 = 'H';
+char letra2 = 'O';
+char letra3 = 'L';
+char letra4 = 'A';
+
+string palabra = "" + letra1 + letra2 + letra3 + letra4;
+
+Console.WriteLine("Palabra formada: " + palabra);
+
+Console.WriteLine("La letra es: " + letra);
+Console.WriteLine("Su código ASCII es: " + codigoAscii);
+
+
 
             Console.WriteLine("\nPresiona ENTER para cerrar.");
             Console.ReadLine();
