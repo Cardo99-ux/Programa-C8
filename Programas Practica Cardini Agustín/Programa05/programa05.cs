2d2d688e2a8d49e836e6d1988561edad417de653
Cardini Agustín <cardiniagustin8@gmail.com>
2026-04-10 18:10:02 -0300

    Actualizar programa05.cs

--- Programas Practica Cardini Agustín/Programa05/programa05.cs
+++ Programas Practica Cardini Agustín/Programa05/programa05.cs
@@ -6,20 +6,51 @@ class Program
 {
     static void Main(string[] args)
     {
-         // Booleano (bool) - Solo true o false
+         //1. Booleano (bool) - Solo true o false
             bool verdadero = true;
             bool falso = false;
-        Console.WriteLine($"--- BLOQUE 4: VALORES LÓGICOS ({verdadero} / {falso}) ---");//1
+        Console.WriteLine($"--- BLOQUE 4: VALORES LÓGICOS ({verdadero} / {falso}) ---");
 
-            Console.WriteLine($"Los valores booleanos se distinguen entre {verdadero} y {falso} , "); //2
+            Console.WriteLine($"Los valores booleanos se distinguen entre {verdadero} y {falso} , ");
             Console.WriteLine($"son la forma de dato primitivas de las computadoras");
-            Console.WriteLine($"cuyo valor 1 indica {verdadero} y 0 indica {falso} "); //3
+            Console.WriteLine($"cuyo valor 1 indica {verdadero} y 0 indica {falso} "); 
             Console.WriteLine(".__________________.");
-            Console.WriteLine($"|1-->{verdadero}         |"); //4
+            Console.WriteLine($"|1-->{verdadero}         |"); 
             Console.WriteLine("|_________________|");
-            Console.WriteLine($"|0-->{falso}        |"); //5
+            Console.WriteLine($"|0-->{falso}        |");
             Console.WriteLine("|_________________|");
            
+//2 ejemplo 
+int numero = 5;
+bool esPositivo = numero > 0;
+
+Console.WriteLine("¿El número es positivo? " + esPositivo);
+
+//3. ejemplo 
+int edad = 20;
+bool esMayorDeEdad = edad >= 18;
+
+Console.WriteLine("¿Es mayor de edad? " + esMayorDeEdad);
+
+//4.ejemplo
+bool luzRoja = true;
+bool luzVerde = false;
+
+if (luzRoja)
+{
+    Console.WriteLine("Detenerse");
+}
+else if (luzVerde)
+{
+    Console.WriteLine("Avanzar");
+}
+//5.ejemplo
+bool tieneUsuario = true;
+bool tieneContraseña = false;
+
+bool accesoPermitido = tieneUsuario && tieneContraseña;
+
+Console.WriteLine("Acceso al sistema: " + accesoPermitido);
            
             Console.WriteLine("\nPresiona ENTER para cerrar.");
             Console.ReadLine();
