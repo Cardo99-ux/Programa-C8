3f9f7b3c19633a6e669f9bfa7053714f2d98cd6d
Cardini Agustín <cardiniagustin8@gmail.com>
2026-04-10 18:59:17 -0300

    Actualizar Program.cs

--- Programas Practica Cardini Agustín/Program00/Programa 00/Program.cs
+++ Programas Practica Cardini Agustín/Program00/Programa 00/Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("-- Test Datos --");
-        Console.WriteLine("Nombre/Apellido: Cardini Agustín");
-        Console.WriteLine("Fecha Nac.: 01/12/1999");
-        Console.WriteLine("Carrera: Estudiante de Programacion Universitario-UTN FRT");
-        Console.WriteLine("Año cursado: 2°");
+        //1 
+Console.WriteLine("-- Test Datos --");
+       //2
+ Console.WriteLine("Nombre/Apellido: Cardini Agustín");
+       //3
+ Console.WriteLine("Fecha Nac.: 01/12/1999");
+       //4
+ Console.WriteLine("Carrera: Estudiante de Programacion Universitario-UTN FRT");
+       //5
+ Console.WriteLine("Año cursado: 2°");
         
     }
 }
